9991d8935f5bf1c852c8159a74a968e784f63c53
Mircea Cozmei <micozmei@hotmail.com>
2020-01-16 15:16:13 -0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -31,7 +31,7 @@ triangular = 0
 for i in range(1, n + 1):
     triangular = triangular + i
 print("Triangular number", n, "via loop:", triangular)
-print("Triangular number", n, "via formula:", n * ( n + 1 ) / 2)
+print("Triangular number", n, "via formula:", n * (n + 1) / 2)
 print()
 
 
